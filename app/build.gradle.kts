495a062883a34a2dcbe8a2b543ee308f8d80b4e7
Malcolm Chris-Ogar <malcolmchris2008@live.co.uk>
2025-08-09 17:20:21 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+
+
+}
+
+android {
+    namespace = "com.example.notesapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.notesapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+
+    val room_version = "2.7.2"
+    implementation ("androidx.room:room-ktx:$room_version")
+
+
+
+
+    ksp("androidx.room:room-compiler:$room_version")
+
+    implementation("androidx.room:room-runtime:${room_version}")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+
+
+
+
+
+
+
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+}
